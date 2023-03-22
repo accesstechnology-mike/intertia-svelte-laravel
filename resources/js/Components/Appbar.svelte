f80b87da550cab5fef3e0f0a18ab5fb4b1d0633f
mike <mike@wsl-vscode-laptop.com>
2023-03-22 23:32:13 +0000

    layout

---
+++ resources/js/Components/Appbar.svelte
@@ -0,0 +1,31 @@
+<script>
+    import { AppBar } from '@skeletonlabs/skeleton';
+    import { LightSwitch } from '@skeletonlabs/skeleton';
+
+    import { drawerStore } from '@skeletonlabs/skeleton';
+
+    function drawerOpen() {
+    drawerStore.open({});
+}
+
+
+</script>
+
+<AppBar>
+    <svelte:fragment slot="lead">
+        <div class="flex items-center">
+            <button class="lg:hidden btn btn-sm mr-4" on:click={drawerOpen}>
+                <span>
+                    <svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
+                        <rect width="100" height="20" />
+                        <rect y="30" width="100" height="20" />
+                        <rect y="60" width="100" height="20" />
+                    </svg>
+                </span>
+            </button>
+            <strong class="text-xl uppercase">access: log</strong>
+        </div>
+    </svelte:fragment>
+	<svelte:fragment slot="trail"><LightSwitch/></svelte:fragment>
+</AppBar>
+
---
+++ resources/js/Components/Sidebar.svelte
@@ -0,0 +1,22 @@
+<script>
+    import { drawerStore } from '@skeletonlabs/skeleton';
+
+    function drawerClose() {
+    drawerStore.close();
+}
+
+</script>
+
+<section class="p-4 pb-20 space-y-4 overflow-y-auto">
+    <nav class="list-nav p-4">
+        <ul>
+            <li>
+                <a href="/elements/lists">
+                    <span class="badge bg-primary-500">💀</span>
+                    <span class="flex-auto">Skeleton</span>
+                </a>
+            </li>
+        </ul>
+    </nav>
+
+</section>
