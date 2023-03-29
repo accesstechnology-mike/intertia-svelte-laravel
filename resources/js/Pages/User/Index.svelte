7cb35dc5e0d371f092056c03fe38edabb677377f
mike <mike@wsl-vscode-laptop.com>
2023-03-30 00:37:53 +0100

    spatie roles etc

--- resources/js/Pages/User/Index.svelte
+++ resources/js/Pages/User/Index.svelte
@@ -28,7 +28,7 @@
                 <div class="flex mt-4 space-x-1 md:mt-4">
                     <!-- Add links or buttons for specific actions -->
                     <a
-                        class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        class="btn variant-soft-secondary"
                         href={`/users/${user.id}/edit`}>Edit</a
                     >
                 </div>
