1e3b62eff65a9c12436e2bb915151e416dc45fda
mike <mike@removedevserver.com>
2023-06-21 20:52:40 +0100

    redirect to new log3 vercel app

--- routes/web.php
+++ routes/web.php
@@ -8,26 +8,30 @@ use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\DashboardController;
 
-// home route
-Route::get('/', function () {
+Route::any('{any}', function () {
+    return redirect('https://log3.vercel.app');
+})->where('any', '.*');
 
-    //if auth redirect to dashboard
-    if (Auth::check()) {
-        return redirect('/dashboard');
-    }
-    //else render welcome page
-    return Inertia::render('Welcome');
-})->name('welcome');
+// // home route
+// Route::get('/', function () {
 
-// auth routes
-Route::get('/login', [AuthController::class, 'login'])->name('login');
-Route::get('/callback', [AuthController::class, 'callback']);
-Route::get('/logout', [AuthController::class, 'logout']);
+//     //if auth redirect to dashboard
+//     if (Auth::check()) {
+//         return redirect('/dashboard');
+//     }
+//     //else render welcome page
+//     return Inertia::render('Welcome');
+// })->name('welcome');
 
-// all other routes require middleware auth group
-Route::middleware('auth')->group(function () {
+// // auth routes
+// Route::get('/login', [AuthController::class, 'login'])->name('login');
+// Route::get('/callback', [AuthController::class, 'callback']);
+// Route::get('/logout', [AuthController::class, 'logout']);
 
-    // dashboard route
-    Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
-    Route::get('/profile', [UserController::class, 'show'])->name('profile');
-});
+// // all other routes require middleware auth group
+// Route::middleware('auth')->group(function () {
+
+//     // dashboard route
+//     Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
+//     Route::get('/profile', [UserController::class, 'show'])->name('profile');
+// });
